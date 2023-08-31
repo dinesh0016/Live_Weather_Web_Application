4413366817c2c229926f79f4ddc48f8930074fa5
Dinesh Patel <p.dinesh@iitg.ac.in>
2023-08-31 18:29:29 +0530

    project

---
+++ Live_Weather_app/src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "0ed28c2910fa0d9de91c9946f6dd047c",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
